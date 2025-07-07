b8a0c4d584202c811087e9b86d99f101d06e1758
ameeraelghamry <ameeraelghamry@gmail.com>
2025-07-07 16:44:02 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className='pattern' />
+
+      <div className='wrapper'>
+        <header>
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle</h1>
+        </header>
+        <p>Search</p>
+      </div>
+
+    </main>
+
+  )
+}
+
+export default App
