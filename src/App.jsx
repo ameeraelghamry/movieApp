5a86bbd500144c198b5a0e85d0dfbf82848d68a0
ameeraelghamry <ameeraelghamry@gmail.com>
2025-07-08 01:02:53 +0300

    index page  frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Search from './assets/components/search.jsx'
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
       <div className='pattern' />
 
       <div className='wrapper'>
         <header>
+          <img src='./hero-img.png' alt='hero banner' />
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle</h1>
+
         </header>
-        <p>Search</p>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
 
     </main>
